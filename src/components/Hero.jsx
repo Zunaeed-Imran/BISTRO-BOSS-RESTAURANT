83775024d2666b0fc6d75c793f2f3615c0cb5b80
imran <zunaeede@gmail.com>
2024-10-15 16:40:34 +0600

    main - bg-image fixing.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="w-screen">
       <div
-        className="relative w-full bg-cover bg-center h-[500px] flex items-start justify-start rounded-md mb-28"
+        className="relative w-full bg-cover bg-center h-[500px] flex items-start justify-start rounded-md mb-28 top-0"
         style={{ backgroundImage: `url(${hero})` }}
       >
         <div className="absolute bottom-4 flex left-1/2">
