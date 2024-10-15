6e0c8ad28663e7dcdb8745e0be98543cda37e30f
imran <zunaeede@gmail.com>
2024-10-15 12:40:13 +0600

    main - add some more image content - add hero image.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-xs'>Hello</h1>
         <Hero/>
       </div>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
-
+import hero from "../assets/pageimage/hero.png";
+import button from "../assets/logo/heroButton.png";
 
 function Hero() {
   return (
     <div>
-      <h1>The hero section.</h1>
+      <div
+        className=" bg-cover bg-center h-[500px] flex items-start justify-start rounded-md mb-28"
+        style={{ backgroundImage: `url(${hero})` }}
+      >
+        <img src={button} alt="hero button" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
