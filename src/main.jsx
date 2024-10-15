13056c2b2524d12fc9cfa1af2a22158bf26c1793
imran <zunaeede@gmail.com>
2024-10-15 10:27:58 +0600

    main - setup the basic layout of this project.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainOutlet from './routes/MainOutlet';
+import ErrorPage from './page/ErrorPage';
+import Navbar from './layout/Navbar'
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    
-  }
+    element: <MainOutlet />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/navbar',
+        element: <Navbar/>
+      },
+    ],
+  },
 ]);
 
 
