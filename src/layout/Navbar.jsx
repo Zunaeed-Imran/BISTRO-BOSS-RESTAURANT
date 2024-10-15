188c1ab91b104b3f79e2228c9d312df4472ddc6b
imran <zunaeede@gmail.com>
2024-10-15 14:47:39 +0600

    main - add content on the navabr - styling the nav content.

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,13 +1,23 @@
-
+import logo from "../assets/logo/logo.png";
 
 function Navbar() {
   return (
     <div>
-      <div>
-        
+      <div className="flex justify-between">
+        <div className="justify items-center">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="flex gap-4 text-white justify items-center">
+          <p>HOME</p>
+          <p>CONTACT US</p>
+          <p>DASHBOARD</p>
+          <p>OUR MENU</p>
+          <p>OUR SHOP</p>
+          <p>SIGN OUT</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
