f8554e7a4d12e1c2ad59dcee4967e60f27180f4c
imran <zunaeede@gmail.com>
2024-10-16 15:52:01 +0600

    main - define the props for chef manu card.

--- src/cards/ChefCard.jsx
+++ src/cards/ChefCard.jsx
@@ -1,11 +1,22 @@
+import PropTytpes from 'prop-types';
 
-
-function ChefCard() {
+function ChefCard({img, name, desc, button}) {
   return (
     <div>
-      
+      <img src={img} alt="manuimg" />
+      <h2>{name}</h2>
+      <p>{desc}</p>
+      {button}
     </div>
   )
 }
 
 export default ChefCard
+
+
+ChefCard.propTypes = {
+  img: PropTytpes.string,
+  name: PropTytpes.text,
+  desc: PropTytpes.text,
+  button: PropTytpes.string,
+};
