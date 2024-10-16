73e02a75448e981e2aeeb7bf2de05177df20e2d7
imran <zunaeede@gmail.com>
2024-10-16 12:56:26 +0600

    main - add copy the manu card couple of time.

--- src/components/FromOurManu.jsx
+++ src/components/FromOurManu.jsx
@@ -8,7 +8,47 @@ function FromOurManu() {
         <p className="italic text-[#D99904] text-xl">---Check it out---</p>
         <h1 className="text-4xl py-4">FROM OUR MENU</h1>
       </div>
-      <div>
+      <div className="grid grid-cols-2">
+        <ManuCard
+          img={ManuLogo}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
         <ManuCard
           img={ManuLogo}
           name={'ROAST DUCK BREAST ------------------'}
