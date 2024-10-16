ea074e4418ffdd5ac1f4539e7053cb08a1220e97
imran <zunaeede@gmail.com>
2024-10-16 15:43:13 +0600

    main - add chef card.

---
+++ src/cards/ChefCard.jsx
@@ -0,0 +1,11 @@
+
+
+function ChefCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChefCard
--- src/components/ChefRecommends.jsx
+++ src/components/ChefRecommends.jsx
@@ -7,6 +7,9 @@ function ChefRecommends() {
         <p className="italic text-[#D99904] text-xl">---Should Try---</p>
         <h1 className="text-4xl py-7">CHEF RECOMMENDS</h1>
       </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
