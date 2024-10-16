a5dad67275d90876dcc86c40ebd9987e45b7919e
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-16 22:07:57 +0600

    main - adding some basic data on the testimonial section - add soem basic styled on the testimonial section.

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,10 +2,13 @@
 
 function Testimonial() {
   return (
-    <div>
-      
+    <div className="py-20">
+      <div>
+        <p className="text-xl text-[#D99904]">---What Our Clients Say---</p>
+        <h1 className="text-4xl py-5">TESTIMONIALS</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Testimonial
