8790a02c7b6e1612d9720af5d6bdd6c8e92a3c82
imran <zunaeede@gmail.com>
2024-10-16 12:23:11 +0600

    main - define all the card from manu card for props type.

--- src/cards/ManuCard.jsx
+++ src/cards/ManuCard.jsx
@@ -1,11 +1,28 @@
+import PropTypes from 'prop-types';
 
-
-function ManuCard() {
+function ManuCard({img, name, desc, price}) {
   return (
     <div>
-      
+      <div>
+        <img src={img} alt="logo image" />
+      </div>
+      <div>
+        <p>{name}</p>
+        <p>{desc}</p>
+      </div>
+      <div>
+        <p>{price}</p>
+      </div>
     </div>
   )
 }
 
 export default ManuCard
+
+
+ManuCard.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  desc: PropTypes.text,
+  price: PropTypes.string,
+};
