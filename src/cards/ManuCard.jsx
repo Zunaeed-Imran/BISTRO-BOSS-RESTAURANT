a2f17e36be7857aaa111c59230efb4fdb7b02572
imran <zunaeede@gmail.com>
2024-10-16 13:08:34 +0600

    main - finally done work - styled the manu card.

--- src/cards/ManuCard.jsx
+++ src/cards/ManuCard.jsx
@@ -2,19 +2,19 @@ import PropTypes from 'prop-types';
 
 function ManuCard({img, name, desc, price}) {
   return (
-    <div className='flex'>
+    <div className="flex gap-3">
       <div>
         <img src={img} alt="logo image" />
       </div>
       <div>
-        <p className='font-cinzel text-xl font-semibold'>{name}</p>
-        <p>{desc}</p>
+        <p className="font-cinzel text-xl font-semibold text-left">{name}</p>
+        <p className='text-left'>{desc}</p>
       </div>
       <div>
-        <p>{price}</p>
+        <p className="text-[#D99904] font-semibold">{price}</p>
       </div>
     </div>
-  )
+  );
 }
 
 export default ManuCard
--- src/components/FromOurManu.jsx
+++ src/components/FromOurManu.jsx
@@ -4,7 +4,7 @@ import ManuLogo from "../assets/logo/textlogo.png";
 function FromOurManu() {
   return (
     <div>
-      <div className="">
+      <div className="pb-10">
         <p className="italic text-[#D99904] text-xl">---Check it out---</p>
         <h1 className="text-4xl py-4">FROM OUR MENU</h1>
       </div>
