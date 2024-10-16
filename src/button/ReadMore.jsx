8f4502b04e90bf28aac6abec24bde176f60a1668
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-16 19:25:50 +0600

    main - add a read more card.

---
+++ src/button/ReadMore.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ReadMore() {
+  return (
+    <div className=''>
+      <button className='btn'>READ MORE</button>
+    </div>
+  )
+}
+
+export default ReadMore
--- src/components/FromOurMenu.jsx
+++ src/components/FromOurMenu.jsx
@@ -1,4 +1,6 @@
 import manubg from "../assets/pageimage/bgBlurManu.png";
+import manucard from "../assets/cardimage/manu.png";
+import ReadMore from "../button/ReadMore";
 
 function FromOurMenu() {
   return (
@@ -9,6 +11,16 @@ function FromOurMenu() {
       <div>
         <p className="text-xl text-[#D99904]">---Check it out---</p>
         <h1 className="text-4xl text-white">FROM OUR MENU</h1>
+      </div>
+      <div>
+        <div>
+          <img src={manucard} alt="Manu card" />
+        </div>
+        <div>
+          <h2></h2>
+          <p></p>
+          <ReadMore/>
+        </div>
       </div>
     </div>
   );
