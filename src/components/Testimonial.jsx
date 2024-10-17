897c30326e3c26c4663f80ef9cb04c0b72c420da
imran <zunaeede@gmail.com>
2024-10-17 10:30:09 +0600

    main - passing the data in the testimonial section.

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,4 +1,6 @@
-
+import { FaStar } from "react-icons/fa";
+import ReviewCard from "../cards/ReviewCard";
+import logoImage from "../assets/logo/quote-left.png";
 
 function Testimonial() {
   return (
@@ -6,6 +8,16 @@ function Testimonial() {
       <div>
         <p className="text-xl text-[#D99904]">---What Our Clients Say---</p>
         <h1 className="text-4xl py-5">TESTIMONIALS</h1>
+      </div>
+      <div>
+        <ReviewCard
+          logo={<FaStar />}
+          img={logoImage}
+          desc={
+            'Various version have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+          }
+          name={'JANE DOE'}
+        />
       </div>
     </div>
   );
