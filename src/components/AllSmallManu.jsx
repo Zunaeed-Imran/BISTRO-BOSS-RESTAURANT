2ef22d07e64e64b50620495d6240d07de39edbcc
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-17 22:39:34 +0600

    main - add rest of the small manu card.

--- src/components/AllSmallManu.jsx
+++ src/components/AllSmallManu.jsx
@@ -163,6 +163,57 @@ function AllSmallManu() {
           }
         />
       </div>
+      {/* rest of the small manu card */}
+      <div className='grid grid-cols-2 gap-3 py-10'>
+        <ManuCard
+          img={ManuLogo}
+          name={"ROAST DUCK BREAST ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={"TUNA NIÇOISE ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={"ESCALOPE DE VEAU ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={"CHICKEN AND WALNUT SALAD ---------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={"FISH PARMENTIER ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={ManuLogo}
+          name={"ROASTED PORK BELLY ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+      </div>
     </div>
   );
 }
