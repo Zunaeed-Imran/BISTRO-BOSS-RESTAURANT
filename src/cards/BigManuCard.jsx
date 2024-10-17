de6784f4e3be39ea9ad4e74bb9ffcb37df5b7fbc
imran <zunaeede@gmail.com>
2024-10-17 17:22:13 +0600

    main - styled the big manu card.

--- src/cards/BigManuCard.jsx
+++ src/cards/BigManuCard.jsx
@@ -4,9 +4,10 @@ import bigManu from "../assets/cardimage/horizontalcard.png";
 function BigManuCard({name, desc}) {
   return (
     <div
-      className='bg-cover bg-center relative h-[400px] justify-center items-center'
+      className='bg-cover bg-center relative h-[400px] justify-center items-center top-0'
       style={{ backgroundImage: `url(${bigManu})` }}>
-      <div className='bg-black bg-opacity-65 absolute justify-center items-center inset-0 m-auto w-3/5 h-2/4'>
+      
+      <div className='bg-black bg-opacity-65 absolute flex flex-col justify-center items-center inset-0 m-auto w-3/5 h-2/4 p-10'>
         <h1 className='text-3xl font-cinzel font-semibold text-white py-3'>{name}</h1>
         <p className='text-white'>{desc}</p>
       </div>
