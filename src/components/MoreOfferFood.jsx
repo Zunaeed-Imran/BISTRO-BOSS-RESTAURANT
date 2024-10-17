24d8f863a9b1c417eb1f3444e35a4f37f2ac17b9
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-17 20:52:58 +0600

    main - passig data into props.

--- src/components/MoreOfferFood.jsx
+++ src/components/MoreOfferFood.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import ManuCard from '../cards/ManuCard'
+import logoimg from "..//assets/logo/textlogo.png";
 
 function MoreOfferFood() {
   return (
     <div>
-      
+      <div>
+        <ManuCard
+          img={logoimg}
+          name={"ROAST DUCK BREAST ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default MoreOfferFood
