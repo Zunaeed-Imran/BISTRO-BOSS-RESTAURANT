742047ddef7fbcec4bb65e554bc9410357d36c12
imran <zunaeede@gmail.com>
2024-10-17 10:42:32 +0600

    main - description the text leading.

--- src/cards/ReviewCard.jsx
+++ src/cards/ReviewCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function ReviewCard({logo, img, desc, name}) {
   return (
-    <div className='flex flex-col gap-3 justify-center items-center p-32'>
+    <div className='flex flex-col gap-5 justify-center items-center p-32'>
       <div className="flex">
         <p className="text-[#CD9003] text-3xl">{logo}</p>
         <p className="text-[#CD9003] text-3xl">{logo}</p>
@@ -11,7 +11,7 @@ function ReviewCard({logo, img, desc, name}) {
         <p className="text-[#CD9003] text-3xl">{logo}</p>
       </div>
       <img src={img} alt="Logo Image" />
-      <p>{desc}</p>
+      <p className='leading-relaxed'>{desc}</p>
       <h1 className="text-[#CD9003] text-2xl">{name}</h1>
     </div>
   );
