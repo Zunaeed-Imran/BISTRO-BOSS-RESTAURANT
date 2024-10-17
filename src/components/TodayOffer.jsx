0336f666fe9589b5033b73ec0e9a5f29e0b4c320
imran <zunaeede@gmail.com>
2024-10-17 15:56:59 +0600

    main - adding some data on the todays offer manu page.

--- src/components/TodayOffer.jsx
+++ src/components/TodayOffer.jsx
@@ -1,4 +1,5 @@
-
+import ManuCard from "../cards/ManuCard";
+import manuImg from "../assets/logo/textlogo.png";
 
 function TodayOffer() {
   return (
@@ -6,6 +7,40 @@ function TodayOffer() {
       <div className="py-12">
         <p className="italic text-[#D99904]">---Dont miss---</p>
         <h1 className="text-4xl">TODAYS OFFER</h1>
+      </div>
+      <div className="grid grid-cols-2">
+        <ManuCard
+          img={manuImg}
+          name={'ROAST DUCK BREAST ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={manuImg}
+          name={'TUNA NIÇOISE ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={manuImg}
+          name={'ESCALOPE DE VEAU ------------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
+        <ManuCard
+          img={manuImg}
+          name={'CHICKEN AND WALNUT SALAD ---------------'}
+          desc={
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
+          }
+          price={'$14.5'}
+        />
       </div>
     </div>
   );
