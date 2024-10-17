6c2d01af40b68929e4bf7a9362882c726158d9d5
imran <zunaeede@gmail.com>
2024-10-17 17:01:22 +0600

    main - styled the background image for mig manu card.

--- src/cards/BigManuCard.jsx
+++ src/cards/BigManuCard.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
-
+import bigManu from "../assets/cardimage/horizontalcard.png";
 
 function BigManuCard({name, desc}) {
   return (
-    <div>
+    <div
+      className='bg-cover bg-center relative h-[500px]'
+      style={{ backgroundImage: `url(${bigManu})` }}>
       <h1>{name}</h1>
       <p>{desc}</p>
     </div>
