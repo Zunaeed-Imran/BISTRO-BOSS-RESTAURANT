dd24cd28ed34429292310cb66fcca84746571478
imran <zunaeede@gmail.com>
2024-10-17 13:58:34 +0600

    main - make the our menu page. - making the hero section of the our menu component.

---
+++ src/components/OurMenuHero.jsx
@@ -0,0 +1,12 @@
+
+function OurMenuHero() {
+  return (
+    <div>
+      <div>
+        <h1>OUR MENU</h1>
+      </div>
+    </div>
+  )
+}
+
+export default OurMenuHero
---
+++ src/page/OurMenu.jsx
@@ -0,0 +1,12 @@
+import OurMenuHero from "../components/OurMenuHero"
+
+
+function OurMenu() {
+  return (
+    <div>
+      <OurMenuHero/>
+    </div>
+  )
+}
+
+export default OurMenu
