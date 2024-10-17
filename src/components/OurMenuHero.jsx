9d734718cbe192fef1874f34751a3c4aae3f55be
imran <zunaeede@gmail.com>
2024-10-17 14:37:45 +0600

    main - trying to center the inner div..

--- src/components/OurMenuHero.jsx
+++ src/components/OurMenuHero.jsx
@@ -1,12 +1,19 @@
+import heroImg from "../assets/pageimage/manu.jpg";
+
 
 function OurMenuHero() {
   return (
-    <div>
-      <div>
-        <h1>OUR MENU</h1>
+    <div
+      className="bg-cover bg-center h-[500px] relative justify-center items-center"
+      style={{ backgroundImage: `url(${heroImg})` }}>
+      <div className="bg-black bg-opacity-65  absolute flex flex-col justify-center items-center w-3/5 h-3/6">
+        <h1 className="text-5xl font-cinzel font-bold text-white">
+          OUR <br /> MENU
+        </h1>
+        <p className="uppercase font-semibold font-cinzel text-white">Would you like to try a dish?</p>
       </div>
     </div>
-  )
+  );
 }
 
 export default OurMenuHero
