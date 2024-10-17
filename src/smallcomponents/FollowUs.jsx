092c6deb91ab0ef1fbca23c34e9e77f064d5b86e
imran <zunaeede@gmail.com>
2024-10-17 11:25:25 +0600

    main - add some logo on the follow us component.

--- src/smallcomponents/FollowUs.jsx
+++ src/smallcomponents/FollowUs.jsx
@@ -1,9 +1,17 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function FollowUs() {
   return (
     <div>
-      <h1>Follow US</h1>
-      <p>Join us on social media</p>
+      <div>
+        <h1>Follow US</h1>
+        <p>Join us on social media</p>
+      </div>
+      <div>
+        <FaFacebookF />
+        <FaInstagram />
+        <FaTwitter />
+      </div>
     </div>
   );
 }
