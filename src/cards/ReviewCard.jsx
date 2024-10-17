f7d601c0412e58a25b340d4a351d4a3ccc8947df
imran <zunaeede@gmail.com>
2024-10-17 10:41:10 +0600

    main - styled the review section.

--- src/cards/ReviewCard.jsx
+++ src/cards/ReviewCard.jsx
@@ -2,13 +2,19 @@ import PropTypes from 'prop-types';
 
 function ReviewCard({logo, img, desc, name}) {
   return (
-    <div>
-      <p>{logo}</p>
+    <div className='flex flex-col gap-3 justify-center items-center p-32'>
+      <div className="flex">
+        <p className="text-[#CD9003] text-3xl">{logo}</p>
+        <p className="text-[#CD9003] text-3xl">{logo}</p>
+        <p className="text-[#CD9003] text-3xl">{logo}</p>
+        <p className="text-[#CD9003] text-3xl">{logo}</p>
+        <p className="text-[#CD9003] text-3xl">{logo}</p>
+      </div>
       <img src={img} alt="Logo Image" />
       <p>{desc}</p>
-      <h1>{name}</h1>
+      <h1 className="text-[#CD9003] text-2xl">{name}</h1>
     </div>
-  )
+  );
 }
 
 export default ReviewCard
