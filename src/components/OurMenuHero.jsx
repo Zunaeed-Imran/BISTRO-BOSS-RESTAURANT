a0ee24c7b4306f162c8a1b3d73856f32208a4eca
imran <zunaeede@gmail.com>
2024-10-17 15:22:54 +0600

    main - finally fix the our menu hero section inner content - center the inner content.

--- src/components/OurMenuHero.jsx
+++ src/components/OurMenuHero.jsx
@@ -4,13 +4,16 @@ import heroImg from "../assets/pageimage/manu.jpg";
 function OurMenuHero() {
   return (
     <div
-      className="bg-cover bg-center h-[500px] relative justify-center items-center"
-      style={{ backgroundImage: `url(${heroImg})` }}>
-      <div className="bg-black bg-opacity-65  absolute flex flex-col justify-center items-center w-3/5 h-3/6">
+      className="bg-cover bg-center h-[500px] relative justify-center top-0"
+      style={{ backgroundImage: `url(${heroImg})` }}
+    >
+      <div className="bg-black bg-opacity-65  absolute flex flex-col justify-center items-center w-3/5 h-3/6 m-auto inset-0">
         <h1 className="text-5xl font-cinzel font-bold text-white">
           OUR <br /> MENU
         </h1>
-        <p className="uppercase font-semibold font-cinzel text-white">Would you like to try a dish?</p>
+        <p className="uppercase font-semibold font-cinzel text-white">
+          Would you like to try a dish?
+        </p>
       </div>
     </div>
   );
