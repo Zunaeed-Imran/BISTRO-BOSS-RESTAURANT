646dd9d8e62afb518b9ebfba90d4a5d2fc158a63
Zunaeed-Imran <zunaeede@gmail.com>
2024-10-17 20:56:52 +0600

    main - passing more data by props.

--- src/components/MoreOfferFood.jsx
+++ src/components/MoreOfferFood.jsx
@@ -5,10 +5,50 @@ import logoimg from "..//assets/logo/textlogo.png";
 function MoreOfferFood() {
   return (
     <div>
-      <div>
+      <div className="grid grid-cols-2">
         <ManuCard
           img={logoimg}
           name={"ROAST DUCK BREAST ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={logoimg}
+          name={"TUNA NIÇOISE ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={logoimg}
+          name={"ESCALOPE DE VEAU ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={logoimg}
+          name={"CHICKEN AND WALNUT SALAD ---------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={logoimg}
+          name={"FISH PARMENTIER ------------------"}
+          desc={
+            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+          }
+          price={"$14.5"}
+        />
+        <ManuCard
+          img={logoimg}
+          name={"ROASTED PORK BELLY ------------------"}
           desc={
             "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
           }
