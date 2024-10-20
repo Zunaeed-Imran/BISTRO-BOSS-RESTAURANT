62ff352297540367879dd27f3e2bf9e21569d1b5
imran <zunaeede@gmail.com>
2024-10-20 13:57:21 +0600

    main - done task - styled the navbar manu.

--- src/components/OurShopManu.jsx
+++ src/components/OurShopManu.jsx
@@ -4,11 +4,21 @@ function OurShopManu() {
   return (
     <div>
       <div className="flex gap-2 text-xl justify-center items-center py-20">
-        <p>SALAD</p>
-        <p>PIZZA</p>
-        <p>SOUPS</p>
-        <p>DESSERT</p>
-        <p>DRINKS</p>
+        <p className="hover:text-[#D99904] hover:underline decoration-double ">
+          SALAD
+        </p>
+        <p className="hover:text-[#D99904] hover:underline decoration-double">
+          PIZZA
+        </p>
+        <p className="hover:text-[#D99904] hover:underline decoration-double">
+          SOUPS
+        </p>
+        <p className="hover:text-[#D99904] hover:underline decoration-double">
+          DESSERT
+        </p>
+        <p className="hover:text-[#D99904] hover:underline decoration-double">
+          DRINKS
+        </p>
       </div>
     </div>
   );
