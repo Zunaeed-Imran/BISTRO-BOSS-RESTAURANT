275e36d74cbd6df1e6cbbc63044955973bf04e56
imran <zunaeede@gmail.com>
2024-10-20 12:18:48 +0600

    main - adding the basic style of the our shop hero section.

--- src/components/HeroOurShop.jsx
+++ src/components/HeroOurShop.jsx
@@ -1,11 +1,13 @@
-
+import ourshop from "../assets/pageimage/shop.png";
 
 function HeroOurShop() {
   return (
-    <div>
-      <div>
+    <div className="" style={{ backgroundImage: `url(${ourshop})` }}>
+      <div className="bg-black bg-opacity-50  text-white">
         <h1 className="text-5xl font-semibold font-cinzel pb-3">OUR SHOP</h1>
-        <p className="text-xl font-semibold font-cinzel">Would you like to try a dish?</p>
+        <p className="text-xl font-semibold font-cinzel">
+          Would you like to try a dish?
+        </p>
       </div>
     </div>
   );
