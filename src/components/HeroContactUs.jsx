06ef6b490da0588fd3aea118943a9b6f4deadb45
imran <zunaeede@gmail.com>
2024-10-21 10:57:04 +0600

    main - adding some content on the hero section of the contact us page.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -1,9 +1,13 @@
+import contactus from "../assets/pageimage/contact.png";
+
 
 
 function HeroContactUs() {
   return (
-    <div>
-      <div>
+    <div
+      className="bg-cover bg-center h-[500px]"
+      style={{ backgroundImage: `url(${contactus})` }}>
+      <div className="bg-black opacity-60 text-white">
         <h1 className="">CONTACT US</h1>
         <p className="">Would you like to try a dish?</p>
       </div>
