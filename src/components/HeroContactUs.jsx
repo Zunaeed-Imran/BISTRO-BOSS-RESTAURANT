d6b905cdb42d6a47f5df55eed35bb90f17f0570b
imran <zunaeede@gmail.com>
2024-10-21 11:08:03 +0600

    main - styling the hero section of the contact us page.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -5,9 +5,9 @@ import contactus from "../assets/pageimage/contact.png";
 function HeroContactUs() {
   return (
     <div
-      className="bg-cover bg-center h-[500px]"
+      className="relative bg-cover bg-center h-[500px]"
       style={{ backgroundImage: `url(${contactus})` }}>
-      <div className="bg-black opacity-60 text-white">
+      <div className="absolute flex flex-col justify-center items-center m-auto inset-0 bg-black opacity-60 w-3/5 h-3/6 text-white">
         <h1 className="text-5xl font-bold font-cinzel">CONTACT US</h1>
         <p className="text-xl font-semibold font-cinzel">Would you like to try a dish?</p>
       </div>
