78aae9c623d6b2553d266c69084818eea8630477
imran <zunaeede@gmail.com>
2024-10-21 11:31:51 +0600

    main - trying to fix the text opacity.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -6,10 +6,13 @@ function HeroContactUs() {
   return (
     <div
       className="relative bg-cover bg-center h-[500px]"
-      style={{ backgroundImage: `url(${contactus})` }}>
-      <div className="absolute flex flex-col justify-center items-center m-auto inset-0 bg-black opacity-60 w-3/5 h-3/6 text-white">
+      style={{ backgroundImage: `url(${contactus})` }}
+    >
+      <div className="bg-black opacity-60 absolute flex flex-col justify-center items-center m-auto inset-0  w-3/5 h-3/6 text-white">
         <h1 className="text-5xl font-bold font-cinzel">CONTACT US</h1>
-        <p className="text-xl font-semibold font-cinzel">Would you like to try a dish?</p>
+        <p className="text-xl font-semibold font-cinzel">
+          Would you like to try a dish?
+        </p>
       </div>
     </div>
   );
