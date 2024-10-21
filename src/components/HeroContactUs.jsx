41b41ca7867364a305712b9273a25ef24260fc97
imran <zunaeede@gmail.com>
2024-10-21 11:36:56 +0600

    main - finally fix the bg-opacity issue.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -8,7 +8,7 @@ function HeroContactUs() {
       className="relative bg-cover bg-center h-[500px]"
       style={{ backgroundImage: `url(${contactus})` }}
     >
-      <div className="bg-black opacity-60 absolute flex flex-col justify-center items-center m-auto inset-0  w-3/5 h-3/6 text-white">
+      <div className="bg-black bg-opacity-60 absolute flex flex-col justify-center items-center m-auto inset-0  w-3/5 h-3/6 text-white">
         <h1 className="text-5xl font-bold font-cinzel">CONTACT US</h1>
         <p className="text-xl font-semibold font-cinzel">
           Would you like to try a dish?
