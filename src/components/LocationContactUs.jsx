0481630ec09b6f3bd8b010ffb8d4c50055d9acdb
imran <zunaeede@gmail.com>
2024-10-21 12:59:17 +0600

    main - add some gap to look good.

--- src/components/LocationContactUs.jsx
+++ src/components/LocationContactUs.jsx
@@ -10,7 +10,7 @@ function LocationContactUs() {
         <p className="italic font-semibold text-[#D99904]">---Visit Us---</p>
         <h1 className="text-4xl py-3">OUR LOCATION</h1>
       </div>
-      <div className="grid grid-cols-3 py-12">
+      <div className="grid grid-cols-3 gap-3 py-12">
         <Phone />
         <Address />
         <WorkingHours/>
