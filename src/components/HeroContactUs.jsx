fe69776d29edd10f34cbec18e3e0b6d816126bab
imran <zunaeede@gmail.com>
2024-10-21 11:03:19 +0600

    main - adding some text styled o the hero section content.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -8,8 +8,8 @@ function HeroContactUs() {
       className="bg-cover bg-center h-[500px]"
       style={{ backgroundImage: `url(${contactus})` }}>
       <div className="bg-black opacity-60 text-white">
-        <h1 className="">CONTACT US</h1>
-        <p className="">Would you like to try a dish?</p>
+        <h1 className="text-5xl font-bold font-cinzel">CONTACT US</h1>
+        <p className="text-xl font-semibold font-cinzel">Would you like to try a dish?</p>
       </div>
     </div>
   );
