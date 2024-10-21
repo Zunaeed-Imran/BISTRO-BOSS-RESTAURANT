20f6a2801f75e6fbeebaf125508f5583e1c31aa7
imran <zunaeede@gmail.com>
2024-10-21 14:47:08 +0600

    main - adding some style in the button.

--- src/button/SendMessage.jsx
+++ src/button/SendMessage.jsx
@@ -2,10 +2,12 @@
 
 function SendMessage() {
   return (
-    <div>
-      
+    <div className="justify-start flex">
+      <button className="text-left items-center btn bg-[#996D29] hover:bg-[#865F24] text-white">
+        Send Message
+      </button>
     </div>
-  )
+  );
 }
 
 export default SendMessage
