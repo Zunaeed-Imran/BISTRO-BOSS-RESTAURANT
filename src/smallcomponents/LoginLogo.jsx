29e3161d188c8eba3c98dfdb277ac0806a17d2e3
imran <zunaeede@gmail.com>
2024-10-22 10:52:25 +0600

    main - styled the logo on the login page.

--- src/smallcomponents/LoginLogo.jsx
+++ src/smallcomponents/LoginLogo.jsx
@@ -4,12 +4,12 @@ import { FaGithub, FaGoogle } from "react-icons/fa";
 
 function LoginLogo() {
   return (
-    <div>
-      <div>
-        <p>Or sign in with</p>
-        <CiFacebook />
-        <FaGoogle />
-        <FaGithub />
+    <div className="pt-3">
+      <p className="py-1">Or sign in with</p>
+      <div className="flex justify-center gap-3">
+        <CiFacebook size={'2rem'} />
+        <FaGoogle size={'2rem'} />
+        <FaGithub size={'2rem'} />
       </div>
     </div>
   );
