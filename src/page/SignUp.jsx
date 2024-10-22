8324054bc2b6abf425aeb555a8bdc61b4b4d87d0
imran <zunaeede@gmail.com>
2024-10-22 11:28:27 +0600

    main - edit the content on the sign up page.

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -7,13 +7,18 @@ function SignUp() {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center bg-[#F1F2F4]">
-        <div className="pt-32 items-center">
-          <img src={signupimg} alt="" />
-        </div>
         <div className="pt-32 items-center justify-center">
           <div className="md:border-2 border-[#D0D0D0] rounded-md md:p-10 md:px-20">
             <form action="" className="flex flex-col gap-4">
-              <h1 className="text-3xl font-bold py-7">Login</h1>
+              <h1 className="text-3xl font-bold py-7">Sign Up</h1>
+              <label htmlFor="" className="font-semibold m-1 text-left">
+                Name
+              </label>
+              <input
+                type="text"
+                placeholder="Your email"
+                className="input input-bordered w-full"
+              />
               <label htmlFor="" className="font-semibold m-1 text-left">
                 Email
               </label>
@@ -30,34 +35,22 @@ function SignUp() {
                 placeholder="Your password"
                 className="input input-bordered w-full"
               />
-              <label
-                htmlFor=""
-                className="font-semibold m-1 text-left text-[#5D5FEF]"
-              >
-                <input
-                  type="text"
-                  placeholder="U A g l u o "
-                  className="input input-bordered w-full"
-                />
-                Reload Captcha
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full"
-              />
               <Link to="/ok" className="btn bg-[#D9B782] text-white">
-                Sign in
+                Sign Up
               </Link>
             </form>
             <p className="py-3">
-              New here? Create a New Account
-              <Link className="link" to="/signup">
-                Sign-Up
+              Already registered? Go to log in
+              <Link className="link" to="/login">
+                Log-in
               </Link>
             </p>
             <LoginLogo />
           </div>
+        </div>
+        {/* image portion fo this page */}
+        <div className="pt-32 items-center">
+          <img src={signupimg} alt="" />
         </div>
       </div>
     </div>
