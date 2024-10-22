723d5c6cce2e9ea8ff7fdcc5e2811ae634eb8f24
imran <zunaeede@gmail.com>
2024-10-22 11:39:24 +0600

    main - add home button on the error page.

--- src/page/ErrorPage.jsx
+++ src/page/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
 
 
 function ErrorPage() {
   return (
     <div>
       <h1 className="text-3xl text-red-500">This page is not Found.</h1>
+      <Link className="btn btn-success" to={'/'}>Back to Home</Link>
     </div>
   )
 }
