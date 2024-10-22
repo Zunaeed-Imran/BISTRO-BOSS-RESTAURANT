b5c10556be93dddccc07e9d42272db9851fa0da6
imran <zunaeede@gmail.com>
2024-10-22 11:42:12 +0600

    main - styled the not found page text.

--- src/page/ErrorPage.jsx
+++ src/page/ErrorPage.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom"
 function ErrorPage() {
   return (
     <div>
-      <h1 className="text-3xl text-red-500">This page is not Found.</h1>
-      <Link className="btn btn-success" to={'/'}>Back to Home</Link>
+      <h1 className="text-5xl font-bold font-cinzel text-red-500">This page is not Found.</h1>
+      <div className=" pt-20">
+        <Link className="btn btn-success" to={'/'}>Back to Home</Link>
+      </div>
     </div>
   )
 }
