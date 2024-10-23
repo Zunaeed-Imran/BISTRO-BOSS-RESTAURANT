8affe03b55b18ccf839bac99f933c898411fb848
imran <zunaeede@gmail.com>
2024-10-23 16:26:33 +0600

    main - responsive the location contact section.

--- src/components/LocationContactUs.jsx
+++ src/components/LocationContactUs.jsx
@@ -6,11 +6,11 @@ import WorkingHours from "../smallcomponents/WorkingHours";
 function LocationContactUs() {
   return (
     <div>
-      <div className="py-12">
-        <p className="italic font-semibold text-[#D99904]">---Visit Us---</p>
+      <div className="py-7">
+        <p className="italic text-xl text-[#D99904]">---Visit Us---</p>
         <h1 className="text-4xl py-3">OUR LOCATION</h1>
       </div>
-      <div className="grid grid-cols-3 gap-3 py-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 py-4">
         <Phone />
         <Address />
         <WorkingHours/>
