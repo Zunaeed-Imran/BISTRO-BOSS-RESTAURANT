6bbc21d73050140505c409b51cfffc568e6e4977
imran <zunaeede@gmail.com>
2024-10-23 15:23:34 +0600

    main - responsive the callus card.

--- src/smallcomponents/CallUs.jsx
+++ src/smallcomponents/CallUs.jsx
@@ -3,7 +3,9 @@
 function CallUs() {
   return (
     <div className="bg-black p-14">
-      <h1 className="font-semibold text-3xl text-white font-cinzel">Call Us: +88 0192345678910</h1>
+      <h1 className="font-semibold text-xl md:text-3xl text-white font-cinzel">
+        Call Us: +88 0192345678910
+      </h1>
     </div>
   );
 }
