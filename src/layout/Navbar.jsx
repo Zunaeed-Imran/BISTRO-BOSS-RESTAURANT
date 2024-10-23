8770a440e4e57dbb6baf728ef76de0bcdd2fe5db
imran <zunaeede@gmail.com>
2024-10-23 14:27:22 +0600

    main - responsive the navbar.

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,6 +2,7 @@ import { CgProfile } from "react-icons/cg";
 import logo from "../assets/logo/logo.png";
 import { RiShoppingCartLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
   return (
@@ -11,24 +12,30 @@ function Navbar() {
           <img src={logo} alt="logo" />
         </div>
         <div className="flex gap-4 text-white font-bold justify items-center pr-20">
-          <Link to="/" className="hover:text-yellow-400">
-            HOME
-          </Link>
-          <Link to="/contactuspage" className="hover:text-yellow-400">
-            CONTACT US
-          </Link>
-          <Link to="/login" className="hover:text-yellow-400">
-            DASHBOARD
-          </Link>
-          <Link to="/ourmenu" className="hover:text-yellow-400">
-            OUR MENU
-          </Link>
-          <Link to="/ourshop" className="hover:text-yellow-400 flex gap-2">
-            OUR SHOP <RiShoppingCartLine size={'1.5rem'} />
-          </Link>
-          <Link to={'/signup'} className="hover:text-yellow-400 flex gap-2">
-            SIGN OUT <CgProfile size={'1.5rem'} />
-          </Link>
+          {/* Button for mobile screen */}
+          <button className="md:hidden">
+            <FaBars />
+          </button>
+          <div className="hidden md:flex gap-4">
+            <Link to="/" className="hover:text-yellow-400">
+              HOME
+            </Link>
+            <Link to="/contactuspage" className="hover:text-yellow-400">
+              CONTACT US
+            </Link>
+            <Link to="/login" className="hover:text-yellow-400">
+              DASHBOARD
+            </Link>
+            <Link to="/ourmenu" className="hover:text-yellow-400">
+              OUR MENU
+            </Link>
+            <Link to="/ourshop" className="hover:text-yellow-400 flex gap-2">
+              OUR SHOP <RiShoppingCartLine size={'1.5rem'} />
+            </Link>
+            <Link to={'/signup'} className="hover:text-yellow-400 flex gap-2">
+              SIGN OUT <CgProfile size={'1.5rem'} />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
