c631156308fce2db3f9113468d65533bfdf4f471
imran <zunaeede@gmail.com>
2024-10-23 16:57:32 +0600

    main - responsive the big manu props passing card.

--- src/cards/BigManuCard.jsx
+++ src/cards/BigManuCard.jsx
@@ -4,15 +4,17 @@ import bigManu from "../assets/cardimage/horizontalcard.png";
 function BigManuCard({name, desc}) {
   return (
     <div
-      className='bg-cover bg-center relative h-[400px] justify-center items-center top-0'
-      style={{ backgroundImage: `url(${bigManu})` }}>
-      
-      <div className='bg-black bg-opacity-65 absolute flex flex-col justify-center items-center inset-0 m-auto w-3/5 h-2/4 p-10'>
-        <h1 className='text-3xl font-cinzel font-semibold text-white py-3'>{name}</h1>
-        <p className='text-white'>{desc}</p>
+      className="bg-cover bg-center relative h-[400px] justify-center items-center top-0"
+      style={{ backgroundImage: `url(${bigManu})` }}
+    >
+      <div className="bg-black bg-opacity-65 absolute flex flex-col justify-center items-center inset-0 m-auto w-4/5 h-3/5 md:h-2/4 p-10">
+        <h1 className="text-3xl font-cinzel font-semibold text-white py-3">
+          {name}
+        </h1>
+        <p className="text-white">{desc}</p>
       </div>
     </div>
-  )
+  );
 }
 
 export default BigManuCard
