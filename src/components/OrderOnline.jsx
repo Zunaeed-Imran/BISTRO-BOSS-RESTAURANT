383be1d8594bcb795df64155c5a423e8b2e2c96f
imran <zunaeede@gmail.com>
2024-10-23 15:20:43 +0600

    main - add some padding to look good the text.

--- src/components/OrderOnline.jsx
+++ src/components/OrderOnline.jsx
@@ -6,7 +6,7 @@ function OrderOnline() {
   return (
     <div className="justify items-center">
       <div>
-        <p className="text-[#D99904] italic">---Form 11:00am to 10:00pm---</p>
+        <p className="text-[#D99904] italic pb-3">---Form 11:00am to 10:00pm---</p>
         <h1 className="text-4xl">ORDER ONLINE</h1>
       </div>
       <div className="justify items-center py-8">
