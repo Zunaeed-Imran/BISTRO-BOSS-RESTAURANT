399adb9a17d8325ae5d0d9e33bf47b2418f69548
imran <zunaeede@gmail.com>
2024-10-23 16:11:13 +0600

    main - responsive the hero section of the contact us page.

--- src/components/HeroContactUs.jsx
+++ src/components/HeroContactUs.jsx
@@ -9,8 +9,8 @@ function HeroContactUs() {
       style={{ backgroundImage: `url(${contactus})` }}
     >
       <div className="bg-black bg-opacity-60 absolute flex flex-col justify-center items-center m-auto inset-0  w-3/5 h-3/6 text-white">
-        <h1 className="text-5xl font-bold font-cinzel">CONTACT US</h1>
-        <p className="text-xl font-semibold font-cinzel">
+        <h1 className="text-3xl md:text-5xl font-bold font-cinzel">CONTACT US</h1>
+        <p className="text-xl font-semibold font-cinzel px-3">
           Would you like to try a dish?
         </p>
       </div>
