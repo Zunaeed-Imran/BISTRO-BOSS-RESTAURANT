d70ed701ab861b0ad8c55b0ddd449c896bd58a90
imran <zunaeede@gmail.com>
2024-10-23 17:49:39 +0600

    main - responsive the sign up page.

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -11,7 +11,7 @@ function SignUp() {
       <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center bg-[#F1F2F4]">
         <div className="pt-32 items-center justify-center">
           <div className="md:border-2 border-[#D0D0D0] rounded-md md:p-10 md:px-20">
-            <form action="" className="flex flex-col gap-4">
+            <form action="" className="flex flex-col gap-4 px-4">
               <h1 className="text-3xl font-bold py-7">Sign Up</h1>
               <label htmlFor="" className="font-semibold m-1 text-left">
                 Name
