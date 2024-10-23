ce8a87589f9dc570d550dc63b208a05e23c8c658
imran <zunaeede@gmail.com>
2024-10-23 16:36:14 +0600

    main - responsive the our menu page hero section.

--- src/components/OurMenuHero.jsx
+++ src/components/OurMenuHero.jsx
@@ -8,7 +8,7 @@ function OurMenuHero() {
       style={{ backgroundImage: `url(${heroImg})` }}
     >
       <div className="bg-black bg-opacity-65  absolute flex flex-col justify-center items-center w-3/5 h-3/6 m-auto inset-0">
-        <h1 className="text-5xl font-cinzel font-bold text-white">
+        <h1 className="text-3xl md:text-5xl font-cinzel font-bold text-white">
           OUR <br /> MENU
         </h1>
         <p className="uppercase font-semibold font-cinzel text-white">
