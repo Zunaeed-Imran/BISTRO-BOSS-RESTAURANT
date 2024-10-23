97d9c136841b85bdc3403540a9f4dd371c11ae04
imran <zunaeede@gmail.com>
2024-10-23 17:02:57 +0600

    main - responsive the more offer food card.

--- src/components/MoreOfferFood.jsx
+++ src/components/MoreOfferFood.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ManuCard from '../cards/ManuCard'
 import logoimg from "..//assets/logo/textlogo.png";
 import Favurate from '../button/Favurate';
@@ -7,7 +6,7 @@ import BigManuCard from '../cards/BigManuCard';
 function MoreOfferFood() {
   return (
     <div>
-      <div className="grid grid-cols-2 gap-3 py-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-5">
         <ManuCard
           img={logoimg}
           name={"ROAST DUCK BREAST ------------------"}
