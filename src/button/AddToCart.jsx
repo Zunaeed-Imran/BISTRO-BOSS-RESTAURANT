6d7cc8828ac46a6b050371effdde170480a28951
imran <zunaeede@gmail.com>
2024-10-28 11:52:18 +0600

    main - added transaction effect in the button.

--- src/button/AddToCart.jsx
+++ src/button/AddToCart.jsx
@@ -3,7 +3,7 @@
 function AddToCart() {
   return (
     <div>
-      <button className="uppercase btn hover:bg-black border-b-4 border-[#D99904] text-[#D99904]">
+      <button className="uppercase btn hover:bg-black border-b-4 border-[#D99904] text-[#D99904] ease-in-out duration-500 hover:scale-105">
         add to cart
       </button>
     </div>
