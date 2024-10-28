121173e27b746b78269854474654ef2d90b4678a
imran <zunaeede@gmail.com>
2024-10-28 17:14:05 +0600

    main - fix the aling and padding on the footer section.

--- src/smallcomponents/FollowUs.jsx
+++ src/smallcomponents/FollowUs.jsx
@@ -2,12 +2,12 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function FollowUs() {
   return (
-    <div className="">
-      <div className="">
+    <div className="flex flex-col justify-center items-center">
+      <div className="justify-center items-center text-center">
         <h1 className="text-2xl pb-5">Follow US</h1>
         <p>Join us on social media</p>
       </div>
-      <div className="flex gap-3 text-3xl pt-4">
+      <div className="flex gap-3 text-3xl pt-4 justify-center items-center">
         <FaFacebookF />
         <FaInstagram />
         <FaTwitter />
