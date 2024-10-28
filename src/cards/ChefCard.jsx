6c81ec060c29a4038809164c0a7e80e1f78a0580
imran <zunaeede@gmail.com>
2024-10-28 11:36:33 +0600

    main - fix the padding and align of the chef card.

--- src/cards/ChefCard.jsx
+++ src/cards/ChefCard.jsx
@@ -4,8 +4,8 @@ function ChefCard({img, name, desc, button}) {
   return (
     <div className="flex flex-col bg-[#F3F3F3] m-3 pb-7">
       <img src={img} alt="manuimg" />
-      <h2 className="text-2xl font-bold py-2">{name}</h2>
-      <p className='py-3 text-left p-5'>{desc}</p>
+      <h2 className="text-2xl font-bold  py-2 p-5">{name}</h2>
+      <p className="py-3  p-5">{desc}</p>
       {button}
     </div>
   );
