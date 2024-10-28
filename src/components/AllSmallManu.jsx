1a33dbab5faf7a355c783f37762b8dec40f5032b
imran <zunaeede@gmail.com>
2024-10-28 14:16:52 +0600

    main - done task - give all image set in the small menu card.

--- src/components/AllSmallManu.jsx
+++ src/components/AllSmallManu.jsx
@@ -1,5 +1,10 @@
 import ManuCard from '../cards/ManuCard'
-import ManuLogo from "../assets/logo/textlogo.png";
+import roastDuck from '../assets/SmallMenu/ROAST DUCK BREAST.png';
+import tune from '../assets/SmallMenu/TUNA NIÇOISE.png';
+import escalape from '../assets/SmallMenu/ESCALOPE DE VEAU .png';
+import chicken from '../assets/SmallMenu/CHICKEN AND WALNUT SALAD.png';
+import fish from '../assets/SmallMenu/FISH PARMENTIER.png';
+import rostPork from '../assets/SmallMenu/ROASTED PORK BELLY.png';
 import Favurate from '../button/Favurate';
 import BigManuCard from '../cards/BigManuCard';
 
@@ -8,76 +13,76 @@ function AllSmallManu() {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-5 md:px-20">
         <ManuCard
-          img={ManuLogo}
-          name={"ROAST DUCK BREAST ------------------"}
+          img={roastDuck}
+          name={'ROAST DUCK BREAST ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"TUNA NIÇOISE ------------------"}
+          img={tune}
+          name={'TUNA NIÇOISE ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ESCALOPE DE VEAU ------------------"}
+          img={escalape}
+          name={'ESCALOPE DE VEAU ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"CHICKEN AND WALNUT SALAD ---------------"}
+          img={chicken}
+          name={'CHICKEN AND WALNUT SALAD ---------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={fish}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ROASTED PORK BELLY ------------------"}
+          img={rostPork}
+          name={'ROASTED PORK BELLY ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={fish}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ROASTED PORK BELLY ------------------"}
+          img={roastDuck}
+          name={'ROASTED PORK BELLY ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={tune}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
       </div>
 
@@ -85,68 +90,68 @@ function AllSmallManu() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 pt-24 md:px-20">
         <ManuCard
-          img={ManuLogo}
-          name={"ROAST DUCK BREAST ------------------"}
+          img={escalape}
+          name={'ROAST DUCK BREAST ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"TUNA NIÇOISE ------------------"}
+          img={chicken}
+          name={'TUNA NIÇOISE ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ESCALOPE DE VEAU ------------------"}
+          img={rostPork}
+          name={'ESCALOPE DE VEAU ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"CHICKEN AND WALNUT SALAD ---------------"}
+          img={rostPork}
+          name={'CHICKEN AND WALNUT SALAD ---------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={fish}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ROASTED PORK BELLY ------------------"}
+          img={rostPork}
+          name={'ROASTED PORK BELLY ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={fish}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ROASTED PORK BELLY ------------------"}
+          img={rostPork}
+          name={'ROASTED PORK BELLY ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
       </div>
       {/* lower button  */}
@@ -154,68 +159,68 @@ function AllSmallManu() {
         <Favurate />
       </div>
       {/* anothe Big Manu Item */}
-      <div className='md:px-20'>
+      <div className="md:px-20">
         <BigManuCard
-          name={"SOUPS"}
+          name={'SOUPS'}
           desc={
-            "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
           }
         />
       </div>
       {/* rest of the small manu card */}
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 py-10 md:px-20'>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 py-10 md:px-20">
         <ManuCard
-          img={ManuLogo}
-          name={"ROAST DUCK BREAST ------------------"}
+          img={roastDuck}
+          name={'ROAST DUCK BREAST ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"TUNA NIÇOISE ------------------"}
+          img={tune}
+          name={'TUNA NIÇOISE ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ESCALOPE DE VEAU ------------------"}
+          img={escalape}
+          name={'ESCALOPE DE VEAU ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"CHICKEN AND WALNUT SALAD ---------------"}
+          img={chicken}
+          name={'CHICKEN AND WALNUT SALAD ---------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"FISH PARMENTIER ------------------"}
+          img={fish}
+          name={'FISH PARMENTIER ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
-          name={"ROASTED PORK BELLY ------------------"}
+          img={rostPork}
+          name={'ROASTED PORK BELLY ------------------'}
           desc={
-            "Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce"
+            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
           }
-          price={"$14.5"}
+          price={'$14.5'}
         />
       </div>
       {/* end of the page fav button */}
-      <div className='py-5'>
-        <Favurate/>
+      <div className="py-5">
+        <Favurate />
       </div>
     </div>
   );
