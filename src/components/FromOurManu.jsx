6ca8b3db56496de3105c40d9c87bf53de42d0711
imran <zunaeede@gmail.com>
2024-10-28 11:25:57 +0600

    main - done task - added some content on the small menu cards - fix the align on the from our menu card.

--- src/components/FromOurManu.jsx
+++ src/components/FromOurManu.jsx
@@ -1,5 +1,10 @@
 import ManuCard from "../cards/ManuCard";
-import ManuLogo from "../assets/logo/textlogo.png";
+import roastDuck from "../assets/SmallMenu/ROAST DUCK BREAST.png";
+import tune from "../assets/SmallMenu/TUNA NIÇOISE.png";
+import escalape from "../assets/SmallMenu/ESCALOPE DE VEAU .png";
+import chicken from "../assets/SmallMenu/CHICKEN AND WALNUT SALAD.png";
+import fish from "../assets/SmallMenu/FISH PARMENTIER.png";
+import rostPork from "../assets/SmallMenu/ROASTED PORK BELLY.png";
 import ViewFullManu from "../button/ViewFullManu";
 import CallUs from "../smallcomponents/CallUs";
 
@@ -12,7 +17,7 @@ function FromOurManu() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:px-20">
         <ManuCard
-          img={ManuLogo}
+          img={roastDuck}
           name={'ROAST DUCK BREAST ------------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
@@ -20,7 +25,7 @@ function FromOurManu() {
           price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
+          img={tune}
           name={'TUNA NIÇOISE ------------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
@@ -28,7 +33,7 @@ function FromOurManu() {
           price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
+          img={escalape}
           name={'ESCALOPE DE VEAU ------------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
@@ -36,7 +41,7 @@ function FromOurManu() {
           price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
+          img={chicken}
           name={'CHICKEN AND WALNUT SALAD ---------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
@@ -44,7 +49,7 @@ function FromOurManu() {
           price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
+          img={fish}
           name={'FISH PARMENTIER ------------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
@@ -52,7 +57,7 @@ function FromOurManu() {
           price={'$14.5'}
         />
         <ManuCard
-          img={ManuLogo}
+          img={rostPork}
           name={'ROASTED PORK BELLY ------------------'}
           desc={
             'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
