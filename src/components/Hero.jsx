17f60131af05c0d213b1c53a05609f9caa6c5c54
imran <zunaeede@gmail.com>
2024-10-29 12:22:08 +0600

    main - finally mke it dynamic - using the daisi ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,17 +12,45 @@ function Hero() {
         style={{ backgroundImage: `url(${hero})` }}
       >
         <div>
-          <div>
-            <img src={menu} alt="menu img" />
+          <div className="carousel w-full h-[500px]">
+            <div id="item1" className="carousel-item w-full">
+              <img
+                src={hero}
+                className="w-full"
+              />
+            </div>
+            <div id="item2" className="carousel-item w-full">
+              <img
+                src={menu}
+                className="w-full"
+              />
+            </div>
+            <div id="item3" className="carousel-item w-full">
+              <img
+                src={contact}
+                className="w-full"
+              />
+            </div>
+            <div id="item4" className="carousel-item w-full">
+              <img
+                src={shop}
+                className="w-full"
+              />
+            </div>
           </div>
-          <div>
-            <img src={shop} alt="shop img" />
-          </div>
-          <div>
-            <img src={contact} alt="contact img" />
-          </div>
-          <div>
-            <img src={hero} alt="hero img" />
+          <div className="flex w-full justify-center gap-2 py-2">
+            <a href="#item1" className="btn btn-xs">
+              1
+            </a>
+            <a href="#item2" className="btn btn-xs">
+              2
+            </a>
+            <a href="#item3" className="btn btn-xs">
+              3
+            </a>
+            <a href="#item4" className="btn btn-xs">
+              4
+            </a>
           </div>
         </div>
       </div>
