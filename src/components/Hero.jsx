cb5eec0dc5afbccc406de2916790c97e61f79340
imran <zunaeede@gmail.com>
2024-10-29 11:55:33 +0600

    main - added some image in the hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import hero from "../assets/pageimage/hero.png";
+import menu from "../assets/pageimage/manu.jpg";
+import contact from "../assets/pageimage/contact.png";
+import shop from "../assets/pageimage/shop.png";
 
 
 function Hero() {
@@ -8,7 +11,20 @@ function Hero() {
         className="relative w-full bg-cover bg-center h-[500px] flex items-start justify-start rounded-md mb-10 top-0"
         style={{ backgroundImage: `url(${hero})` }}
       >
-
+        <div>
+          <div>
+            <img src={menu} alt="menu img" />
+          </div>
+          <div>
+            <img src={shop} alt="shop img" />
+          </div>
+          <div>
+            <img src={contact} alt="contact img" />
+          </div>
+          <div>
+            <img src={hero} alt="hero img" />
+          </div>
+        </div>
       </div>
     </div>
   );
