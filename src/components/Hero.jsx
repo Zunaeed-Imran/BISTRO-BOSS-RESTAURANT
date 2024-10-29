33aa4de960bf820b625c91efbec9bf5e0914a74d
imran <zunaeede@gmail.com>
2024-10-29 09:57:52 +0600

    main - remove the unnecessary image form the hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import hero from "../assets/pageimage/hero.png";
-import button from "../assets/logo/heroButton.png";
+
 
 function Hero() {
   return (
@@ -8,9 +8,7 @@ function Hero() {
         className="relative w-full bg-cover bg-center h-[500px] flex items-start justify-start rounded-md mb-10 top-0"
         style={{ backgroundImage: `url(${hero})` }}
       >
-        <div className="absolute bottom-4 flex left-1/2">
-          <img src={button} alt="hero button" />
-        </div>
+
       </div>
     </div>
   );
