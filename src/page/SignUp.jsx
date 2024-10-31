e6785da7de816b4deb42ca9e50201b6da7e75538
imran <zunaeede@gmail.com>
2024-10-31 16:34:15 +0600

    main - box sizing the login and sign up page.

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -9,7 +9,7 @@ function SignUp() {
     <div>
       <PageTitle title={'SignUp'} />
       <div className="grid w-full grid-cols-1 md:grid-cols-2 justify-center items-center bg-[#F1F2F4]">
-        <div className="md:pt-32 md:p-8 items-center justify-center">
+        <div className="md:pt-32 md:p-20 items-center justify-center">
           <div className="md:border-2 border-[#D0D0D0] rounded-md md:p-10 md:px-20">
             <form action="" className="flex flex-col gap-4 px-4">
               <h1 className="text-3xl font-bold py-7">Sign Up</h1>
