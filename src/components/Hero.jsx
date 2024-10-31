be00210be9c02cb8c75462ac2c727627e82ca379
imran <zunaeede@gmail.com>
2024-10-31 12:06:45 +0600

    main - change the styled of the sero section carosel.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,44 +13,62 @@ function Hero() {
       >
         <div>
           <div className="carousel w-full h-[500px]">
-            <div id="item1" className="carousel-item w-full">
+            <div id="slide1" className="carousel-item relative w-full">
               <img
                 src={hero}
                 className="w-full"
               />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-            <div id="item2" className="carousel-item w-full">
+            <div id="slide2" className="carousel-item relative w-full">
               <img
                 src={menu}
                 className="w-full"
               />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-            <div id="item3" className="carousel-item w-full">
+            <div id="slide3" className="carousel-item relative w-full">
               <img
                 src={contact}
                 className="w-full"
               />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-            <div id="item4" className="carousel-item w-full">
+            <div id="slide4" className="carousel-item relative w-full">
               <img
                 src={shop}
                 className="w-full"
               />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide3" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide1" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="flex w-full justify-center gap-2 py-2">
-            <a href="#item1" className="btn btn-xs">
-              1
-            </a>
-            <a href="#item2" className="btn btn-xs">
-              2
-            </a>
-            <a href="#item3" className="btn btn-xs">
-              3
-            </a>
-            <a href="#item4" className="btn btn-xs">
-              4
-            </a>
           </div>
         </div>
       </div>
