67254e7a9421a037b340114fe88dc070471f685a
imran <zunaeede@gmail.com>
2024-10-31 21:18:27 +0600

    main - added some new section for the future.

---
+++ src/smallcomponents/PizzaSection.jsx
@@ -0,0 +1,62 @@
+import AddToCart from '../button/AddToCart';
+import chifManu from '../assets/cardimage/card.png';
+import ChefCard from '../cards/ChefCard';
+
+function PizzaSection() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 pb-10 px-20">
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PizzaSection;
---
+++ src/smallcomponents/SoupSection.jsx
@@ -0,0 +1,63 @@
+import AddToCart from "../button/AddToCart";
+import chifManu from '../assets/cardimage/card.png';
+import ChefCard from "../cards/ChefCard";
+
+
+function SoupSection() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 pb-10 px-20">
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SoupSection
