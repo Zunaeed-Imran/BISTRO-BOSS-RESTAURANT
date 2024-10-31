533a8bd946f05cb150fda13fbbe585f5f11081be
imran <zunaeede@gmail.com>
2024-10-31 21:09:03 +0600

    main - setup the file structure for making the tab button.

--- src/components/OurShopManu.jsx
+++ src/components/OurShopManu.jsx
@@ -1,7 +1,6 @@
-import ChefCard from "../cards/ChefCard";
-import chifManu from '../assets/cardimage/card.png';
-import AddToCart from "../button/AddToCart";
+
 import NavButton from "../button/NavButton";
+import SaladSection from "../smallcomponents/SaladSection";
 
 function OurShopManu() {
   return (
@@ -9,56 +8,7 @@ function OurShopManu() {
       {/* nav button of this page */}
         <NavButton/>
       {/* start the card section here */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 pb-10 px-20">
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-        <ChefCard
-          img={chifManu}
-          name={'Caeser Salad'}
-          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
-          button={<AddToCart />}
-        />
-      </div>
+        <SaladSection/>
     </div>
   );
 }
---
+++ src/smallcomponents/SaladSection.jsx
@@ -0,0 +1,62 @@
+import ChefCard from "../cards/ChefCard";
+import chifManu from '../assets/cardimage/card.png';
+import AddToCart from '../button/AddToCart';
+
+function SaladSection() {
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 pb-10 px-20">
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+        <ChefCard
+          img={chifManu}
+          name={'Caeser Salad'}
+          desc={'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}
+          button={<AddToCart />}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SaladSection
