77ce46eb45bca681f2fb0f4913d67118479da2ec
imran <zunaeede@gmail.com>
2024-11-07 15:06:25 +0600

    main - added data in the pizza section.

--- src/smallcomponents/PizzaSection.jsx
+++ src/smallcomponents/PizzaSection.jsx
@@ -1,5 +1,5 @@
 import AddToCart from '../button/AddToCart';
-import chifManu from '../assets/cardimage/card.png';
+import chifManu from '../assets/cardimage/pizzaAll.png';
 import ChefCard from '../cards/ChefCard';
 
 function PizzaSection() {
