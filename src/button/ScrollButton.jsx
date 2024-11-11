0807cbe78972a195084b88702d49fb9e427a456d
imran <zunaeede@gmail.com>
2024-11-11 17:06:16 +0600

    main - setup the functionality of the scroll to top.

--- src/button/ScrollButton.jsx
+++ src/button/ScrollButton.jsx
@@ -2,10 +2,18 @@ import { LiaHandPointUp } from "react-icons/lia";
 
 
 function ScrollButton() {
+  
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div>
+    <button onClick={scrollToTop}>
       <LiaHandPointUp size={'2rem'} color="white"/>
-    </div>
+    </button>
   );
 }
 
