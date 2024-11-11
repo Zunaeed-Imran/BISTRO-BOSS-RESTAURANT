69f990ea53e673635eeb68cb4ae4261576304591
imran <zunaeede@gmail.com>
2024-11-11 13:14:11 +0600

    main - setup the scroll

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,36 +1,35 @@
-import { CgProfile } from "react-icons/cg";
-import logo from "../assets/logo/logo.png";
-import { RiShoppingCartLine } from "react-icons/ri";
-import { Link as RouterLink } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
+import logo from '../assets/logo/logo.png';
+import { RiShoppingCartLine } from 'react-icons/ri';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
-import { FaBars } from "react-icons/fa";
-import { useState } from "react";
-import { IoMdLogIn } from "react-icons/io";
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+import { IoMdLogIn } from 'react-icons/io';
 
 function Navbar() {
-// toggle nav option.
   const [mobile, setMobile] = useState(false);
+  const location = useLocation(); // Get the current path
 
-  // nav visibility opetion.
   const toggleMobile = () => {
     setMobile(!mobile);
   };
 
-
   return (
-    <div className=" bg-black  top-0 fixed w-full z-20 p-0 m-0">
+    <div className="bg-black top-0 fixed w-full z-20 p-0 m-0">
       <div className="flex justify-between w-full px-20">
         <div className="justify items-center">
-          <ScrollLink
-            to="home"
-            smooth={true}
-            duration={500}
-          >
-            <img src={logo} alt="logo" />
-          </ScrollLink>
+          {location.pathname === '/' ? (
+            <ScrollLink to="home" smooth={true} duration={500}>
+              <img src={logo} alt="logo" />
+            </ScrollLink>
+          ) : (
+            <RouterLink to="/">
+              <img src={logo} alt="logo" />
+            </RouterLink>
+          )}
         </div>
         <div className="flex gap-4 text-white font-bold justify items-center">
-          {/* Button for mobile screen */}
           <button className="md:hidden" onClick={toggleMobile}>
             <FaBars />
           </button>
@@ -55,19 +54,19 @@ function Navbar() {
               to="/ourshop"
               className="hover:text-yellow-400 flex gap-2"
             >
-              OUR SHOP <RiShoppingCartLine size={'1.5rem'} />
+              OUR SHOP <RiShoppingCartLine size="1.5rem" />
             </RouterLink>
             <RouterLink
-              to={'/login'}
+              to="/login"
               className="hover:text-yellow-400 flex gap-2"
             >
-              LOGIN <IoMdLogIn size={'1.5rem'} />
+              LOGIN <IoMdLogIn size="1.5rem" />
             </RouterLink>
             <RouterLink
-              to={'/signup'}
+              to="/signup"
               className="hover:text-yellow-400 flex gap-2"
             >
-              SIGN UP <CgProfile size={'1.5rem'} />
+              SIGN UP <CgProfile size="1.5rem" />
             </RouterLink>
           </div>
         </div>
@@ -76,7 +75,6 @@ function Navbar() {
   );
 }
 
-export default Navbar
-
+export default Navbar;
 
 // bg - transparent; it is transparent class to make transparent.
