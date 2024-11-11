443211fc12e2a7ba95e2b9219c694b9133d5d209
imran <zunaeede@gmail.com>
2024-11-11 10:24:31 +0600

    main - remove the navbar underline.

--- src/button/NavButton.jsx
+++ src/button/NavButton.jsx
@@ -7,37 +7,37 @@ function NavButton() {
       <div className="flex gap-4 lg:gap-10 text-xl justify-center items-center pt-10 pb-7">
         <Link
           to={'allsection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           AllMenu
         </Link>
         <Link
           to={'saladsection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           SALAD
         </Link>
         <Link
           to={'pizzasection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           PIZZA
         </Link>
         <Link
           to={'soupsection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           SOUPS
         </Link>
         <Link
           to={'dessertsection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           DESSERT
         </Link>
         <Link
           to={'drinksection'}
-          className="hover:text-[#D99904] hover:underline decoration-double btn"
+          className="hover:text-[#D99904] decoration-double btn"
         >
           DRINKS
         </Link>
