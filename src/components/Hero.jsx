d559b0d4451181c6520ba3d5adbb657cd5743e37
imran <zunaeede@gmail.com>
2024-11-12 16:27:04 +0600

    main - setup the slicker gallary.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,11 @@ function Hero() {
     var settings = {
       dots: true,
       infinite: true,
-      speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true,
     };
 
 
