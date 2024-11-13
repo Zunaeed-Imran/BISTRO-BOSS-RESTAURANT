db0a25227dbf394050e3ce4a3c97fb9db8d4e42b
imran <zunaeede@gmail.com>
2024-11-13 12:15:41 +0600

    main - set transaction in online order card.

--- src/cards/OrderOnlineCard.jsx
+++ src/cards/OrderOnlineCard.jsx
@@ -6,16 +6,16 @@ import dissert from "../assets/cardimage/dessert.png";
 function OrderOnlineCard() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5 lg:grid-cols-4 lg:gap-20 px-20">
-      <div className="link hover:scale-105 hover:opacity-50">
+      <div className="link hover:scale-105 hover:opacity-50 hover:transition-opacity">
         <img src={salad} alt="salad" />
       </div>
-      <div className="link hover:scale-105 hover:opacity-50">
+      <div className="link hover:scale-105 hover:opacity-50 hover:transition-opacity">
         <img src={pizza} alt="pizza" />
       </div>
-      <div className="link hover:scale-105 hover:opacity-50">
+      <div className="link hover:scale-105 hover:opacity-50 hover:transition-opacity">
         <img src={soup} alt="soup" />
       </div>
-      <div className="link hover:scale-105 hover:opacity-50">
+      <div className="link hover:scale-105 hover:opacity-50 hover:transition-opacity">
         <img src={dissert} alt="desserts" />
       </div>
     </div>
