c352e8c0cad47089939a43cdf844cca758d11d0f
imran <zunaeede@gmail.com>
2024-11-17 12:03:39 +0600

    main - set the paginaiton on - from our menu section.

--- src/components/FromOurManu.jsx
+++ src/components/FromOurManu.jsx
@@ -1,4 +1,3 @@
-import ManuCard from "../cards/ManuCard";
 import roastDuck from "../assets/SmallMenu/ROAST DUCK BREAST.png";
 import tune from "../assets/SmallMenu/TUNA NIÇOISE.png";
 import escalape from "../assets/SmallMenu/ESCALOPE DE VEAU .png";
@@ -6,8 +5,86 @@ import chicken from "../assets/SmallMenu/CHICKEN AND WALNUT SALAD.png";
 import fish from "../assets/SmallMenu/FISH PARMENTIER.png";
 import rostPork from "../assets/SmallMenu/ROASTED PORK BELLY.png";
 import ViewFullManu from "../button/ViewFullManu";
+import ManuCardPagination from "../cards/ManuCardPagination";
 
 function FromOurManu() {
+
+    const menuItems = [
+      {
+        img: roastDuck,
+        name: 'ROAST DUCK BREAST ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: tune,
+        name: 'TUNA NIÇOISE ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: escalape,
+        name: 'ESCALOPE DE VEAU ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: chicken,
+        name: 'CHICKEN AND WALNUT SALAD ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: fish,
+        name: 'FISH PARMENTIER ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: rostPork,
+        name: 'ROASTED PORK BELLY ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: roastDuck,
+        name: 'ROAST DUCK BREAST ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: tune,
+        name: 'TUNA NIÇOISE ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: escalape,
+        name: 'ESCALOPE DE VEAU ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: chicken,
+        name: 'CHICKEN AND WALNUT SALAD ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: fish,
+        name: 'FISH PARMENTIER ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      {
+        img: rostPork,
+        name: 'ROASTED PORK BELLY ---',
+        desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+        price: '$14.5',
+      },
+      // Add more items as needed
+    ];
+
   return (
     <div>
       <div className="">
@@ -16,55 +93,8 @@ function FromOurManu() {
         </p>
         <h1 className="text-4xl pb-10">FROM OUR MENU</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:px-20">
-        <ManuCard
-          img={roastDuck}
-          name={'ROAST DUCK BREAST ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={tune}
-          name={'TUNA NIÇOISE ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={escalape}
-          name={'ESCALOPE DE VEAU ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={chicken}
-          name={'CHICKEN AND WALNUT SALAD ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={fish}
-          name={'FISH PARMENTIER ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={rostPork}
-          name={'ROASTED PORK BELLY ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
+      <div className="md:grid-cols-2 gap-3 md:px-20">
+        <ManuCardPagination items={menuItems}/>
       </div>
       <div className="py-10">
         <ViewFullManu />
