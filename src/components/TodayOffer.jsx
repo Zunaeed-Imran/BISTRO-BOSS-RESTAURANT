54ae2a66ced6a8dc4d6569b6785754a1e4ef0f95
imran <zunaeede@gmail.com>
2024-11-17 12:08:57 +0600

    main - todays offer menu section added pagination.

--- src/components/TodayOffer.jsx
+++ src/components/TodayOffer.jsx
@@ -1,51 +1,73 @@
-import ManuCard from "../cards/ManuCard";
-import rostDuck from '../assets/SmallMenu/ROAST DUCK BREAST.png';
-import tuna from "../assets/SmallMenu/TUNA NIÇOISE.png";
-import escalpe from '../assets/SmallMenu/ESCALOPE DE VEAU .png';
+import roastDuck from '../assets/SmallMenu/ROAST DUCK BREAST.png';
+import tune from '../assets/SmallMenu/TUNA NIÇOISE.png';
+import escalape from '../assets/SmallMenu/ESCALOPE DE VEAU .png';
 import chicken from '../assets/SmallMenu/CHICKEN AND WALNUT SALAD.png';
 import Favurate from "../button/Favurate";
 import BigManuCard from "../cards/BigManuCard";
+import ManuCardPagination from "../cards/ManuCardPagination";
 
 function TodayOffer() {
+
+      const menuItems = [
+        {
+          img: roastDuck,
+          name: 'ROAST DUCK BREAST ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: tune,
+          name: 'TUNA NIÇOISE ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: escalape,
+          name: 'ESCALOPE DE VEAU ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: chicken,
+          name: 'CHICKEN AND WALNUT SALAD ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: roastDuck,
+          name: 'ROAST DUCK BREAST ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: tune,
+          name: 'TUNA NIÇOISE ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: escalape,
+          name: 'ESCALOPE DE VEAU ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        {
+          img: chicken,
+          name: 'CHICKEN AND WALNUT SALAD ---',
+          desc: 'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce',
+          price: '$14.5',
+        },
+        // Add more items as needed
+      ];
+
   return (
     <div>
       <div className="py-10">
         <p className="italic font-semibold text-[#D99904] pb-3">---Dont miss---</p>
         <h1 className="text-4xl">TODAYS OFFER</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:px-20">
-        <ManuCard
-          img={rostDuck}
-          name={'ROAST DUCK BREAST ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={tuna}
-          name={'TUNA NIÇOISE ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={escalpe}
-          name={'ESCALOPE DE VEAU ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
-        <ManuCard
-          img={chicken}
-          name={'CHICKEN AND WALNUT SALAD ---'}
-          desc={
-            'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'
-          }
-          price={'$14.5'}
-        />
+      <div className="md:grid-cols-2 gap-10 md:px-20">
+        <ManuCardPagination items={menuItems}/>
       </div>
       <div className="py-10">
         <Favurate />
