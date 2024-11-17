6db9d44512d7a6ce54f4ab7e7ea52ec9fc641766
imran <zunaeede@gmail.com>
2024-11-17 15:01:20 +0600

    main - finally fix the style of the tab menu.

--- src/components/OurShopTabs.jsx
+++ src/components/OurShopTabs.jsx
@@ -7,26 +7,51 @@ import PizzaSection from '../smallcomponents/PizzaSection';
 import SoupSection from '../smallcomponents/SoupSection';
 import DessertSection from '../smallcomponents/DessertSection';
 import DrinkSection from '../smallcomponents/DrinkSection';
-// import '../assets/styles/OurSHopTabMenu.css';
 
 const OurShopTabs = () => {
   const [tabIndex, setTabIndex] = useState(0);
 
   return (
     <div className="">
-      <Tabs
-        className=""
-        selectedIndex={tabIndex}
-        onSelect={index => setTabIndex(index)}
-      >
+      <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
         {/* Tab List */}
-        <TabList className={'flex gap-4 lg:gap-10 text-xl justify-center items-center pt-10 pb-7'}>
-          <Tab>All Menu</Tab>
-          <Tab>Salad</Tab>
-          <Tab>Pizza</Tab>
-          <Tab>Soup</Tab>
-          <Tab>Dessert</Tab>
-          <Tab>Drinks</Tab>
+        <TabList className="flex gap-4 lg:gap-10 text-xl justify-center items-center pt-10 pb-7">
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            All Menu
+          </Tab>
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            Salad
+          </Tab>
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            Pizza
+          </Tab>
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            Soup
+          </Tab>
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            Dessert
+          </Tab>
+          <Tab
+            className="btn hover:text-[#D99904]"
+            selectedClassName="text-[#D99904] font-bold btn"
+          >
+            Drinks
+          </Tab>
         </TabList>
 
         {/* Tab Panels */}
