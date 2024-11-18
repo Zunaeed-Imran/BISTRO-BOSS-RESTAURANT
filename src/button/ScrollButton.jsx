bf61671762f3e9036fcee0a302da7ea8e3f3e33e
imran <zunaeede@gmail.com>
2024-11-18 14:58:10 +0600

    main - set the visible function.

--- src/button/ScrollButton.jsx
+++ src/button/ScrollButton.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import { LiaHandPointUp } from "react-icons/lia";
 
 
 function ScrollButton() {
+
+  const [visable, setVisable] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const heroHight = document.getElementById('hero').clientHeight;
+
+      // condation, show button if the hero section end.
+      if (window.screenY > heroHight) {
+        setVisable(true);
+      } else {
+        setVisable(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   
   const scrollToTop = () => {
     window.scroll({
@@ -11,9 +32,20 @@ function ScrollButton() {
   };
 
   return (
-    <button onClick={scrollToTop}>
-      <LiaHandPointUp size={'2rem'} color="white"/>
-    </button>
+    <div>
+      {/* hero section */}
+      <section id="hero"></section>
+
+      {/* scroll to top button */}
+      {visable && (
+        <button
+          className="z-50 bottom-5 right-7 fixed bg-black p-2 rounded-md"
+          onClick={scrollToTop}
+        >
+          <LiaHandPointUp size={'1.5rem'} color="white" />
+        </button>
+      )}
+    </div>
   );
 }
 
