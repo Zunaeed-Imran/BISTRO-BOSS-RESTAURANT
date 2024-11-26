7f6f9935fb8448b748dc1de08bb0f615577afcb3
imran <zunaeede@gmail.com>
2024-11-26 16:23:51 +0600

    main - added data in the json file. - also create the api display file.

---
+++ src/components/LocationContactAPI.jsx
@@ -0,0 +1,14 @@
+
+
+function LocationContactAPI() {
+  return (
+    <div>
+      <div className="pt-10">
+        <p className="italic text-xl text-[#D99904] pb-3">---Visit Us---</p>
+        <h1 className="text-4xl pb-10">OUR LOCATION</h1>
+      </div>
+    </div>
+  );
+}
+
+export default LocationContactAPI
